a398b82ebbe6d0eeecfe7b14b9054204a8d5d2f2
opilane <opilane@HP-CZC6297B9K>
2020-09-09 15:28:39 +0300

    Добавил метод Tap, теперь при нажатии на Frame, меняется надпись самого Frame'a, текст увеличивается 100 раз. Изменил стиль.

--- MobileAppPanchenko/MobileAppPanchenko/Valgusfoor2.xaml.cs
+++ MobileAppPanchenko/MobileAppPanchenko/Valgusfoor2.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,26 +14,28 @@ namespace MobileAppPanchenko
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Valgusfoor2 : ContentPage
     {
+        Label punane, kollane, roheline;
+        Frame pun, roh, kol;
+        Button btn, valja;
+        bool btn_valja;
         public Valgusfoor2()
         {
             //InitializeComponent();
-            Label punane = new Label()
+            punane = new Label()
             {
                 Text = "Punane",
                 TextColor = Color.White,
                 FontSize = 30,
                 FontAttributes = FontAttributes.Bold
             };
-            Frame pun = new Frame()
+            pun = new Frame()
             {
                 BackgroundColor = Color.Red,
                 Content = punane,
                 CornerRadius = 60,
                 Margin = new Thickness(80, 0, 80, 0),
-                VerticalOptions = VerticalOptions.CenterAndExpand,
-                HorizontalOptions = HorizontalOptions.CenterAndExpand,
             };
-            Label kollane = new Label()
+            kollane = new Label()
             {
                 Text = "Kollane",
                 TextColor = Color.White,
@@ -39,47 +43,112 @@ namespace MobileAppPanchenko
                 FontAttributes = FontAttributes.Bold,
 
             };
-            Frame kol = new Frame()
+            kol = new Frame()
             {
                 BackgroundColor = Color.Yellow,
                 Content = kollane,
                 CornerRadius = 60,
                 Margin = new Thickness(80, 0, 80, 0),
-                VerticalOptions = VerticalOptions.CenterAndExpand,
-                HorizontalOptions = HorizontalOptions.CenterAndExpand,
             };
-            Label roheline = new Label()
+            roheline = new Label()
             {
                 Text = "Roheline",
                 TextColor = Color.White,
                 FontSize = 30,
                 FontAttributes = FontAttributes.Bold
             };
-            Frame roh = new Frame()
+            roh = new Frame()
             {
                 BackgroundColor = Color.Green,
                 Content = roheline,
                 CornerRadius = 60,
                 Margin = new Thickness(80, 0, 80, 0),
-                VerticalOptions = VerticalOptions.CenterAndExpand,
-                HorizontalOptions = HorizontalOptins.CenterAndExpand,
             };
-            Button btn=new Button()
+            btn = new Button()
             {
                 Text = "Вкл",
-                VerticalOptions = "StartAndExpand"
-                HorizontalOptions = "StartAndExpand"
-            }
+                BackgroundColor = Color.Gray,
+                BorderColor=Color.Blue,
+            };
+            valja = new Button()
+            {
+                BorderColor = Color.Blue,
+                BackgroundColor = Color.Gray,
+                Text="Выкл",
+            };
             StackLayout stackLayout = new StackLayout()
             {
                 Children =
-                {
-                    pun, kol, roh
-                }
+                    {
+                        pun, kol, roh, btn, valja
+                    }
             };
-            //stackLayout.Orientation = StackOrientation.Horizontal;
+            btn.Clicked += btn_Clicked;
+            valja.Clicked += Valja_Clicked;
+            var tap = new TapGestureRecognizer();
+            tap.Tapped += Tap_Tapped;
+            pun.GestureRecognizers.Add(tap);
+            var tap2 = new TapGestureRecognizer();
+            tap2.Tapped += Tap_Tapped;
+            kol.GestureRecognizers.Add(tap2);
+            var tap3 = new TapGestureRecognizer();
+            tap3.Tapped += Tap_Tapped;
+            roh.GestureRecognizers.Add(tap3);
             Content = stackLayout;
+        }
+
+        private void Valja_Clicked(object sender, EventArgs e)
+        {
+            pun.BackgroundColor = Color.FromRgb(100, 100, 100);
+            kol.BackgroundColor = Color.Gray;
+            roh.BackgroundColor = Color.FromHex("#808080");
+            btn_valja = true;
+        }
+        Random rnd=new Random();
+        private async void btn_Clicked(object sender, EventArgs e)
+        {
+            pun.BackgroundColor = Color.Red;
+            pun.BorderColor = Color.White;
+            btn_valja = false;
+            for(int i=0; i<100; i++)
+            {
+                punane.FontSize++;
+                await Task.Run(() => Thread.Sleep(1000));
+            }
+            int c = rnd.Next(1, 4);
+            if (c == 1)
+            {
+                pun.BackgroundColor = Color.Red;
+                kol.BackgroundColor = Color.FromRgb(62, 62, 62);
+                roh.BackgroundColor = Color.FromRgb(62, 62, 62);
+
+            }
+            else if (c == 2)
+            {
+                pun.BackgroundColor = Color.FromRgb(62, 62, 62);
+                kol.BackgroundColor = Color.Yellow;
+                roh.BackgroundColor = Color.FromRgb(62, 62, 62);
+            }
+            else if (c == 3)
+            {
+                pun.BackgroundColor = Color.FromRgb(62, 62, 62);
+                kol.BackgroundColor = Color.FromRgb(62, 62, 62);
+                roh.BackgroundColor = Color.Green;
+            }
+        }
+
+
+        private void Tap_Tapped(object sender, EventArgs e)
+        {
+            Frame fr = sender as Frame;
+            if (fr == pun) { punane.Text = "Cтой"; };
+            if (fr == kol) { kollane.Text = "Подожди"; };
+            if (fr == roh) { roheline.Text = "Иди "; };
+            if (pun.BackgroundColor==Color.FromRgb(100, 100, 100)) {punane.Text = "Сначала включи"; kollane.Text = "Сначала включи"; roheline.Text = "Сначала включи"; };
+            if (kol.BackgroundColor==Color.Gray) {punane.Text = "Сначала включи"; kollane.Text = "Сначала включи"; roheline.Text = "Сначала включи"; };
+            if (roh.BackgroundColor==Color.FromHex("#808080")) {punane.Text = "Сначала включи"; kollane.Text = "Сначала включи"; roheline.Text = "Сначала включи"; };
 
         }
+        
     }
 }
